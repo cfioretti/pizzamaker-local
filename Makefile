d284ae830f1e4d27ff3a1dd698f3c055b59943cc
Claudio Fioretti <claudio.fioretti@docebo.com>
2025-03-03 01:03:25 +0100

    fix folder structure

--- Makefile
+++ Makefile
@@ -1,9 +1,8 @@
 setup:
-	sh ./scripts/setup.sh
+	./scripts/setup.sh
 
 start:
 	docker-compose up -d
-
 
 stop:
 	docker-compose down -v --remove-orphans
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,10 @@
+#!/usr/bin/env bash
 set -e
 
-cd .. && git clone https://github.com/cfioretti/pizzamaker-fe || echo "Repo already exists" && \
-git clone https://github.com/cfioretti/recipe-manager.git || echo "Repo already exists" && \
-git clone https://github.com/cfioretti/ingredients-balancer || echo "Repo already exists" && \
+cd ..
+git clone https://github.com/cfioretti/pizzamaker-fe || echo "pizzamaker-fe repo already exists"
+git clone https://github.com/cfioretti/recipe-manager.git || echo "recipe-manager repo already exists"
+git clone https://github.com/cfioretti/ingredients-balancer || echo "ingredients-balancer repo already exists"
 cd pizzamaker-local
 
 docker-compose up -d --build
