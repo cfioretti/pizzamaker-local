0c9f2d8dbb17423a94f5dcf63990bfcb73669172
Claudio Fioretti <claudio.fioretti@docebo.com>
2025-03-03 01:03:25 +0100

    fix folder structure

--- script/setup.sh
+++
@@ -1,8 +0,0 @@
-set -e
-
-cd .. && git clone https://github.com/cfioretti/pizzamaker-fe || echo "Repo already exists" && \
-cd .. && git clone https://github.com/cfioretti/recipe-manager.git || echo "Repo already exists" && \
-cd .. && git clone https://github.com/cfioretti/ingredients-balancer || echo "Repo already exists" && \
-cd .. /pizzamaker-local
-
-docker-compose up -d --build
