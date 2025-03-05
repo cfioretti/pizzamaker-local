c94c8cd30fd2d6cf8de940260f9370fa735bb0a6
Claudio Fioretti <claudio.fioretti@docebo.com>
2025-03-05 23:41:34 +0100

    execute docker-compose --build command only on updates

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 set -e
+changes_detected=0
 
 cd repos
-cd pizzamaker-fe && git pull && cd ..
-cd recipe-manager && git pull && cd ..
-cd ingredients-balancer && git pull && cd ..
+check_pull_output() {
+    if ! git pull | grep -q "Already up to date"; then
+        changes_detected=1
+    fi
+}
+
+cd pizzamaker-fe && check_pull_output && cd ..
+cd recipe-manager && check_pull_output && cd ..
+cd ingredients-balancer && check_pull_output && cd ..
 
 docker-compose up -d --build
+if [ $changes_detected -eq 1 ]; then
+    docker-compose up -d --build
+fi
